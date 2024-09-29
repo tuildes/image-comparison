a10b70eaf87881195132ee21812e84668c3eaed2
tuildes <frehsegustavo@gmail.com>
2024-09-29 18:13:44 -0300

    feat: novos testes de LBP

--- lbp.c
+++ lbp.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <math.h>
 
+#define EXIT_FAILURE 1
+#define EXIT_SUCCESS 0
+
 // Retorna a DISTANCIA EUCLIDIANA entre dois vetores de tamanho LENGHT
 double euclidian_distance(int *v1, int *v2, size_t lenght) {
     double res;
@@ -13,7 +16,58 @@ double euclidian_distance(int *v1, int *v2, size_t lenght) {
     return sqrt(res);
 }
 
+/*
+int pgm2_to_lbp(FILE *pgm, FILE *lbp, 
+                size_t width, size_t height, size_t max) { 
+
+    return EXIT_SUCCESS; 
+} */
+unsigned char print_local(unsigned char *vector, unsigned char comp) {
+    unsigned char res = 0, j = 1;
+
+    for (short int i = 0; i < 8; i++) {
+        if (vector[i] > comp) res += j;
+        j *= 2;
+    }
+
+    return res;
+}
+
 int main (/* int argc, char *argv[] */) {
+
+    FILE *original, *new;
+
+    char typeP[10];
+    size_t width, height;
+    short unsigned int max;
+
+    unsigned char localBinary[8] = {
+        1, 1, 1,
+        1, 1, // Deveria ter um no meio
+        1, 1, 1
+    };
+
+    original = fopen("test/imageP2.pgm", "r");
+    new = fopen("test/imageP2.lbp.pgm", "w+");
+
+    if(original == NULL) {
+        perror("Erro ao abrir arquivo PGM");
+        return EXIT_FAILURE;
+    }
+
+    if(new == NULL) {
+        perror("Erro ao criar arquivo LBP");
+        return EXIT_FAILURE;
+    }
+
+    fscanf(original, "%10s", typeP);
+    fscanf(original, "%lu %lu %hu", &width, &height, &max);
+
+    printf("Type: %s - %lu,%lu - max:(%hu)\n", typeP, width, height, max);
+
+    printf("LSB: %u\n", print_local(localBinary, 0));
+
+    fclose(original);
 
     // -i input
     // -d directory base com as imagens
@@ -36,5 +90,5 @@ int main (/* int argc, char *argv[] */) {
     // SEM SAIDAS
     // Erros => nao criar a imagem
 
-    return 0;
+    return EXIT_SUCCESS;
 }
